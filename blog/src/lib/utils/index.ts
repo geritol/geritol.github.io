d71c192543f211e2e80a882ed7e065bedb266b25
Gergo Tolnai <gergo.tolnai@gmail.com>
2024-12-13 18:30:37 +0100

    setup base path

--- blog/src/lib/utils/index.ts
+++ blog/src/lib/utils/index.ts
@@ -1,3 +1,5 @@
+import { base } from '$app/paths';
+
 export const fetchMarkdownPosts = async () => {
     const allPostFiles = import.meta.glob('/src/routes/posts/**/+*.md');
     const iterablePostFiles = Object.entries(allPostFiles);
@@ -5,7 +7,7 @@ export const fetchMarkdownPosts = async () => {
     const allPosts = await Promise.all(
         iterablePostFiles.map(async ([path, resolver]) => {
             const { metadata } = await resolver();
-            const postPath = path.slice(11, -9);
+            const postPath = `${base}${path.slice(11, -9)}`;
 
             return {
                 meta: metadata,
